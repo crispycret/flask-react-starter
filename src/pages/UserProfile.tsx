458d9cd898897701cba6ad9d15b7be66832a6d35
brandon nadeau <bnadeau@uncc.edu>
2022-05-25 23:59:15 -0400

    Added notes of what to do next in the file src/pages/UserProfile.tsx this must start with making changes to the authentication helper upon logging in and ferching user data for the current logged in user. This user cannot view accounts of those they are blocking and those who are blocking them.

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -4,6 +4,11 @@ import {PropsInterface} from 'helpers/interfaces';
 
 export const UserProfile = (props: PropsInterface) => {
 
+    // CURRENT TO DO.
+    // AFTER IMPLEMENT FOLLOW AND BLOCKING AND UPDATE THIS PAGE.
+    // Finally implement a direct messaging feature.
+    // This should be all for a complete version 1.
+    // version 2 should include all the same features but be redesigned for simplicity and readablity.
 
     return (
         <div id='user-profile'>
